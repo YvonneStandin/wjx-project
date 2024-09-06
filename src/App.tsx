6eb726a499b57d46e9bee71ca7b98b99fb0ca09f
YvonneHe <xiaoheiheide@gmail.com>
2024-09-06 17:49:10 +0800

    feat: 自定义hook useGetInfo

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 import List1 from './list1'
 // import StateDemo from './StateDemo'
 import useMouse from './hooks/useMouse'
+import useGetInfo from './hooks/useGetInfo'
 
 function App() {
   const { x, y } = useMouse()
+  const { loading, info } = useGetInfo()
   return (
     <>
       <List1></List1>
@@ -14,6 +16,7 @@ function App() {
         鼠标坐标：
         {x}，{y}
       </p>
+      {loading ? <p>加载中...</p> : <p>{info}</p>}
     </>
   )
 }
