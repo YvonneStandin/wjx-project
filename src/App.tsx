0c9bb23521135edc325664603f6ea041f9df8d41
hemiao <hem@isyscore.com>
2024-09-06 16:05:14 +0800

    feat: hooks useMouse

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './App.css'
 import List1 from './list1'
 // import StateDemo from './StateDemo'
+import useMouse from './hooks/useMouse'
 
 function App() {
+  const { x, y } = useMouse()
   return (
     <>
       <List1></List1>
       {/* <StateDemo></StateDemo> */}
+      <p>
+        {x}
+        {y}
+      </p>
     </>
   )
 }
---
+++ src/hooks/useMouse.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+function useMouse() {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+
+  const mouseMoveHandler = (event: MouseEvent) => {
+    setX(event.clientX)
+    setY(event.clientY)
+  }
+  //调用useMouse的组件，完成渲染后执行
+  useEffect(() => {
+    //监听鼠标事件
+    window.addEventListener('mousemove', mouseMoveHandler)
+    //组件销毁时，解绑dom事件，以防内存泄漏
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    }
+  }, [])
+
+  return { x, y }
+}
+
+export default useMouse
