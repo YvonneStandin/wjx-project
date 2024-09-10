1dcc2ef8a882b9888815396c328d524ae9013cb8
YvonneHe <xiaoheiheide@gmail.com>
2024-09-10 20:02:20 +0800

    refactor: antd重构MainLayoutt

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,24 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
-// import styles from './MainLayout.module.scss'
+import { Layout } from 'antd'
+import styles from './MainLayout.module.scss'
+
+const { Header, Footer, Content } = Layout
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>zuoce</div>
+        <div className={styles.right}>youice</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet></Outlet>
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>
+        夸克奶酪问卷 &copy; 2024 - present. Created by Yvonne
+      </Footer>
+    </Layout>
   )
 }
 
