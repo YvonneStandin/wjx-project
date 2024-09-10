cd4dd0a6dff192343cb93f322288787efe1087a9
YvonneHe <xiaoheiheide@gmail.com>
2024-09-10 23:42:07 +0800

    feat: Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,27 @@
 import React, { FC } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import { Button } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { Button, Typography } from 'antd'
+import { LIST_PATHNAME } from '../router'
+import styles from './Home.module.scss'
+
+const { Title, Paragraph } = Typography
 
 const Home: FC = () => {
   const nav = useNavigate()
   function loginHandler() {
-    // nav('/login')
     nav({
-      pathname: '/login',
-      search: 'b=20',
+      pathname: LIST_PATHNAME,
     })
   }
   return (
-    <div>
-      <p>Home</p>
-      <Button onClick={loginHandler}>登录</Button>
-      <Link to="/register">注册</Link>
+    <div className={styles.container}>
+      <Title>问卷调查 ｜ 在线投票</Title>
+      <Paragraph>已累计创建问卷 101 份，发布分卷 90 份，收到答卷 980 份</Paragraph>
+      <div>
+        <Button type="primary" size="large" onClick={loginHandler}>
+          开始使用
+        </Button>
+      </div>
     </div>
   )
 }
