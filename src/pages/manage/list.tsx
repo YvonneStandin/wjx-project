c1786789ab71eb48d590b7345e907b0617f8f3f4
YvonneHe <xiaoheiheide@gmail.com>
2024-09-20 22:42:22 +0800

    feat: getQuestionListService

--- src/pages/manage/list.tsx
+++ src/pages/manage/list.tsx
@@ -1,76 +1,41 @@
-import React, { FC, useState } from 'react'
-import { produce } from 'immer'
-import { useSearchParams } from 'react-router-dom'
-import { useTitle } from 'ahooks'
-import { Typography } from 'antd'
+import React, { FC } from 'react'
+// import { produce } from 'immer'
+import { useTitle, useRequest } from 'ahooks'
+import { Typography, Spin } from 'antd'
+import { getQuestionListService } from '../../services/question'
 import styles from './common.module.scss'
 import QuestionCard from '../../components/QuestionCard'
 import ListSearch from '../../components/ListSearch'
 
-const rawQuestionList = [
-  {
-    _id: '1',
-    title: '问卷小黑',
-    isPublished: false,
-    isStar: false,
-    createAt: '4月19日 13:00',
-    answerCount: 0,
-  },
-  {
-    _id: '2',
-    title: '问卷小红',
-    isPublished: true,
-    isStar: true,
-    createAt: '6月01日 009:00',
-    answerCount: 0,
-  },
-  {
-    _id: '3',
-    title: '问卷奶酪',
-    isPublished: false,
-    isStar: false,
-    createAt: '4月19日 13:00',
-    answerCount: 0,
-  },
-  {
-    _id: '4',
-    title: '问卷夸克',
-    isPublished: false,
-    isStar: false,
-    createAt: '4月19日 13:00',
-    answerCount: 0,
-  },
-]
-
 const List: FC = () => {
   useTitle('夸克奶酪问卷-我的问卷')
-  const [searchParams] = useSearchParams()
-  console.log(searchParams.get('keyWord'))
 
   const { Title } = Typography
 
-  const [questionList, setQuestionList] = useState(rawQuestionList)
+  //list直接使用，无需再useState
+  const { data = {}, loading } = useRequest(getQuestionListService)
+  const { list: questionList = [] } = data
 
   //删除问卷
-  function deleteQuestion(id: string) {
-    setQuestionList(
-      produce(draft => {
-        draft.splice(
-          questionList.findIndex(item => item._id === id),
-          1
-        )
-      })
-    )
-  }
+  // function deleteQuestion(id: string) {
+  // setQuestionList(
+  //   produce(draft => {
+  //     draft.splice(
+  //       questionList.findIndex(item => item._id === id),
+  //       1
+  //     )
+  //   })
+  // )
+  // }
 
   //发布问卷
-  function publishQuestion(id: string) {
-    setQuestionList(
-      produce(draft => {
-        draft[questionList.findIndex(item => item._id === id)].isPublished = true
-      })
-    )
-  }
+  // function publishQuestion(id: string) {
+  // setQuestionList(
+  //   produce(draft => {
+  //     draft[questionList.findIndex(item => item._id === id)].isPublished = true
+  //   })
+  // )
+  // }
 
   return (
     <>
@@ -83,15 +48,22 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.content}>
-        {questionList.length > 0 &&
-          questionList.map(question => {
+        {loading && (
+          <div className={styles.loading}>
+            <Spin></Spin>
+          </div>
+        )}
+        {!loading &&
+          questionList.length > 0 &&
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          questionList.map((question: any) => {
             const { _id } = question
             return (
               <QuestionCard
                 key={_id}
                 {...question}
-                deleteQuestion={deleteQuestion}
-                publishQuestion={publishQuestion}
+                // deleteQuestion={deleteQuestion}
+                // publishQuestion={publishQuestion}
               />
             )
           })}
