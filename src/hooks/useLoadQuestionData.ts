0b75d94d1493b7df3c6b7c6d8a62878693db25da
YvonneHe <xiaoheiheide@gmail.com>
2024-09-20 21:22:12 +0800

    refactor: 引入useRequest重构useLoadQuestionData

--- src/hooks/useLoadQuestionData.ts
+++ src/hooks/useLoadQuestionData.ts
@@ -1,24 +1,32 @@
-import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useRequest } from 'ahooks'
 import { getQuestionService } from '../services/question'
 
 function useLoadQuestionData() {
   //跟随调用位置
   const { id = '' } = useParams()
 
-  const [loading, setLoading] = useState(true)
-  const [questionData, setQuestionData] = useState({})
+  //不用useRequest需要自行设定loading，data等数据，并在组件加载完成时调用接口执行实现立马触发
+  // const [loading, setLoading] = useState(true)
+  // const [questionData, setQuestionData] = useState({})
 
-  useEffect(() => {
-    async function fn() {
-      const data = await getQuestionService(id)
-      setQuestionData(data)
-      setLoading(false)
-    }
-    fn()
-  }, [])
+  // useEffect(() => {
+  //   async function fn() {
+  //     const data = await getQuestionService(id)
+  //     setQuestionData(data)
+  //     setLoading(false)
+  //   }
+  //   fn()
+  // }, [])
 
-  return { loading, questionData }
+  //异步函数中执行接口调用，并返回接口数据
+  async function loadQuestion() {
+    const data = await getQuestionService(id)
+    return data
+  }
+  //useRequest参数为异步函数
+  const { data, error, loading } = useRequest(loadQuestion)
+  return { loading, data, error }
 }
 
 export default useLoadQuestionData
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -2,9 +2,9 @@ import React, { FC } from 'react'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
 
 const Edit: FC = () => {
-  const { loading, questionData } = useLoadQuestionData()
+  const { loading, data } = useLoadQuestionData()
 
-  return <div>{loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p>}</div>
+  return <div>{loading ? <p>loading</p> : <p>{JSON.stringify(data)}</p>}</div>
 }
 
 export default Edit
