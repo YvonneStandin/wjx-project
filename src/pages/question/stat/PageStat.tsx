6db1c22e847309218d376c7fd5a0aa87ae507e79
YvonneHe <xiaoheiheide@gmail.com>
2024-12-17 12:36:13 +0800

    feat: 问卷统计/表头选中数据联动

--- src/pages/question/stat/PageStat.tsx
+++ src/pages/question/stat/PageStat.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useRequest } from 'ahooks'
-import { Spin, Typography, Table } from 'antd'
+import { Typography, Table } from 'antd'
 import { getStatListService } from '../../../services/stat'
 import useGetQuestionComponentsInfo from '../../../hooks/useGetQuestionComponentsInfo'
 import styles from './PageStat.module.scss'
@@ -14,15 +14,8 @@ type PropsType = {
   setSelectedType: (type: string) => void
 }
 
-const LoadingElem = (
-  <div style={{ textAlign: 'center', marginTop: 60 }}>
-    <Spin />
-  </div>
-)
-
 const PageStat: FC<PropsType> = props => {
   const { selectedId, setSelectedId, setSelectedType } = props
-  console.log(selectedId, setSelectedId, setSelectedType)
   const { id: questionId = '' } = useParams()
 
   const [total, setTotal] = useState(0)
@@ -45,20 +38,45 @@ const PageStat: FC<PropsType> = props => {
   // 创建 table 表头
   const { componentList } = useGetQuestionComponentsInfo()
   const columns = componentList.map(c => {
-    const { fe_id, title } = c
+    const { fe_id, title, props, type } = c
+    const conTitle = props.title || title
+    const color = fe_id === selectedId ? '#1890ff' : 'inherit'
     return {
-      title,
+      title: () => (
+        <Title
+          style={{ fontSize: 14, cursor: 'pointer', margin: 0, color }}
+          onClick={() => handleChangeSelected(fe_id, type)}
+        >
+          {conTitle}
+        </Title>
+      ),
       dataIndex: fe_id,
+      width: 90,
     }
   })
 
-  console.log(columns)
+  function handleChangeSelected(fe_id: string, type: string) {
+    setSelectedId(fe_id)
+    setSelectedType(type)
+  }
+
+  // 创建 table 表体
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dataSource = list.map((i: any) => {
+    return { ...i, key: i._id }
+  })
 
   return (
     <div className={styles.container}>
       <Title level={3}>问卷数量：{!loading && total}</Title>
-      {loading && LoadingElem}
-      {!loading && <Table columns={columns} dataSource={list} />}
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={false}
+        scroll={{ y: 55 * 10 }}
+        loading={loading}
+        size="small"
+      />
     </div>
   )
 }
