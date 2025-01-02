262925d9fede6c45bdd9fcb25f9a0cc619e338fa
YvonneHe <xiaoheiheide@gmail.com>
2025-01-02 18:07:39 +0800

    perf: 抽离antd和react，http缓存

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,37 @@ module.exports = {
       '/api': 'http://localhost:3001', // Mock
     },
   },
+  webpack: {
+    configure(webpackConfig) {
+      if (webpackConfig.mode === 'production') {
+        // 抽离公共代码，只在生产环境
+        if (webpackConfig.optimization == null) {
+          webpackConfig.optimization = {}
+        }
+        webpackConfig.optimization.splitChunks = {
+          chunks: 'all',
+          cacheGroups: {
+            // antd 和 reactDom 比较大单独抽离，否则会在 vendors-chunk 中
+            antd: {
+              name: 'antd-chunk',
+              test: /antd/,
+              priority: 100,
+            },
+            reactDom: {
+              name: 'reactDom-chunk',
+              test: /react-dom/,
+              priority: 99,
+            },
+            // 其它的第三方插件
+            vendors: {
+              name: 'vendors-chunk',
+              test: /node_modules/,
+              priority: 98,
+            },
+          },
+        }
+      }
+      return webpackConfig
+    },
+  },
 }
